0612789118010680dba006a86f164c1775b53c6d
Lexie Choi <lexiechoi852@gmail.com>
2023-10-05 11:26:34 -0400

    feat: added svg icons

---
+++ src/components/icons/DownIcon.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function DownIcon() {
+  return (
+    <svg
+      className="h-6 w-6 text-gray-800 dark:text-white"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 10 12"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="m1 7 4 4 4-4M1 1l4 4 4-4"
+      />
+    </svg>
+  );
+}
---
+++ src/components/icons/UpIcon.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function UpIcon() {
+  return (
+    <svg
+      className="h-6 w-6 text-gray-800 dark:text-white"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 10 12"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M9 5 5 1 1 5m8 6L5 7l-4 4"
+      />
+    </svg>
+  );
+}
