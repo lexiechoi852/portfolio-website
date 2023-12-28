0cf6b821598c53e3d53f08b0e8f38e6da7b66032
Lexie Choi <lexiechoi852@gmail.com>
2023-12-27 23:21:37 -0500

    feat: updated next script strategy

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Script from "next/script";
 import React from "react";
 
@@ -11,11 +13,12 @@ export default function GoogleAnalytics({
   return (
     <>
       <Script
-        async
+        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
       />
       <Script
         id="google-analytics"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
           window.dataLayer = window.dataLayer || [];
